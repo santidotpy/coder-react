3c32f28e0fdbdafd1b9449df419074f9fce9db36
santidotpy <santidotpy@gmail.com>
2022-12-01 02:16:02 -0300

    ✨ add pagination component

---
+++ react-ecommerce/src/components/Paginacion/Paginacion.js
@@ -0,0 +1,12 @@
+import { Pagination } from "@mui/material";
+
+const Paginacion = () => {
+    return (
+        <div>
+            <Pagination count={10} shape="rounded" size="large" />
+
+        </div>
+    );
+}
+
+export default Paginacion;
