22ea8c34701f85cd24ffe996f1e758d768edfa41
santidotpy <santidotpy@gmail.com>
2023-01-07 23:02:15 -0300

    update app

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -1,26 +1,36 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-// import Meli from './components/MockApi/Meli';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createContext } from 'react';
+import { CartProvider } from './context/CartContext';
+import Cart from './components/Cart/Cart';
+import { NotificationProvider } from './context/NotificationContext';
+import Checkout from './components/Checkout/Checkout';
 
+export const CartContext = createContext();
 function App() {
+  
   return (
     <div className="App">
-      <BrowserRouter>
-      <Navbar />
-        <Routes>
-          <Route path="/" element={<ItemListContainer greeting="Welcome Back!" />} /> {/* se encarga de obtener datos para listar */}
-          <Route path="/category/:categoryId" element={<ItemListContainer greeting="Welcome Back!" />} />
-          <Route path="/item/:itemId" element={<ItemDetailContainer />} /> {/* detalle de un producto*/}
+      <CartProvider>
+        <NotificationProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
 
+            <Route path="/" element={<ItemListContainer greeting="Welcome Back!" />} /> {/* se encarga de obtener datos para listar */}
+            <Route path="/category/:categoryId" element={<ItemListContainer greeting="Welcome Back!" />} />
+            <Route path="/item/:itemId" element={<ItemDetailContainer />} /> {/* detalle de un producto*/}
+            <Route path="/cart" element={<Cart />} />
+            <Route path='/checkout' element={<Checkout />} />
 
+          </Routes>
+        </BrowserRouter>
+        </NotificationProvider>
+      </CartProvider>
 
-        </Routes>
-      </BrowserRouter>
-
-      {/* <ItemListContainer greeting="Welcome Back!" /> */}
       {/* <Meli /> */}
     </div>
   );
